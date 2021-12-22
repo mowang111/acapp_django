148e14ed17611e4b499278039601c3d82e0c75c5
mowang111 <1154795004@qq.com>
2021-12-22 15:53:44 +0800

    write my first page

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,7 @@
 from djando.urls import path
-from game.views import index
+from game.views import index,play
 
 urlpatterns = [
     path("",index,name='index'),
+    path("play/",index,name='index'),
 ]

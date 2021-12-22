10860343d94a95dd478b1556349defdf587d1ca5
mowang111 <1154795004@qq.com>
2021-12-22 20:30:48 +0800

    add js html __init__.py

--- acapp/urls.py
+++ acapp/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-#from game.views import index,play
+from django.urls import path,include
 
 urlpatterns = [
-   # path('',index,name='index'),
+    #path('',include('game.urls.index')),
     path('admin/', admin.site.urls),
-   # path('play/',play,name='play'),
 ]
